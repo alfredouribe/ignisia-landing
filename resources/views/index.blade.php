0fe13af83233b993b0131013b330cb3aee1c2e66
Alfredo Uribe <alfredo.uribe.sanchez@gmail.com>
2026-06-21 11:53:39 -0600

    Fix: reserved laravel word deleted

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,7 +19,7 @@
 @push('schema')
 <script type="application/ld+json">
 {
-    "@context": "https://schema.org",
+    "@@context": "https://schema.org",
     "@graph": [
         {
             "@type": "WebSite",
--- resources/views/subscription.blade.php
+++ resources/views/subscription.blade.php
@@ -271,7 +271,7 @@
 @push('schema')
 <script type="application/ld+json">
 {
-    "@context": "https://schema.org",
+    "@@context": "https://schema.org",
     "@graph": [
         {
             "@type": "WebPage",
